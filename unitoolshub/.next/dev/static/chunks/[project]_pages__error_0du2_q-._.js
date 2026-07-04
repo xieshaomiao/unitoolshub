744eeb196500fab61795055ffe9257c6cc24d4d8
谢绍渺 <xieshaomiao@xieshaomiaos-MacBook-Air.local>
2026-07-04 19:42:57 +0800

    chore: clean generated files

--- unitoolshub/.next/dev/static/chunks/[project]_pages__error_0du2_q-._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/1hdb_next_dist_compiled_1ns96j0._.js","static/chunks/1hdb_next_dist_shared_lib_0wdlnth._.js","static/chunks/1hdb_next_dist_client_1frop3b._.js","static/chunks/[root-of-the-server]__1ngibpg._.js","static/chunks/[next]_entry_page-loader_ts_0cxcjwu._.js","static/chunks/162a_react-dom_1hsf_h5._.js","static/chunks/node_modules__pnpm_20tl2_t._.js","static/chunks/[root-of-the-server]__02sxxph._.js"],
-    source: "entry"
-});
